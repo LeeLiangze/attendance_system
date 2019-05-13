870d1d55e528dfae78df292f63834d10dd099e69
LeeLiangze <liangzelee@gmail.com>
2019-05-13 16:00:17 +0800

    Update redis

--- config/database.php
+++ config/database.php
@@ -122,7 +122,7 @@ return [
         'cluster' => false,
 
         'default' => [
-            'host'     => 'redis',
+            'host'     => '127.0.0.1',
             'port'     => 6379,
             'database' => 0,
         ],
