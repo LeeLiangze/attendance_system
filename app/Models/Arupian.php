6cd7af04b6d897e1026a21404aee079e6d9ffa24
LeeLiangze <liangzelee@gmail.com>
2019-11-08 15:16:03 +0800

    Update qrcode private string to staff_id

--- app/Models/Arupian.php
+++ app/Models/Arupian.php
@@ -44,7 +44,7 @@ class Arupian extends MyBaseModel
             while (Arupian::where('reference', $token)->first());
 
             do {
-                $token_private = $arupian->staff_id . "." . explode("@", $arupian->email)[0];
+                $token_private = sprintf("%06d",$arupian->staff_id);
             }
             while (Arupian::where('private_reference', $token_private)->first());
             $arupian->reference = $token;
--- resources/views/Public/ViewEvent/Partials/PDFQRCode.blade.php
+++ resources/views/Public/ViewEvent/Partials/PDFQRCode.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <div style="position:absolute;top:0.10in;left:0.08in;width:0.86in;height:0.86in">
-        {!! DNS2D::getBarcodeSVG($arupian->private_reference, "QRCODE", 3, 3) !!}
+        {!! DNS2D::getBarcodeSVG($arupian->private_reference, "QRCODE", 4, 4) !!}
     </div>
     {{--<img style="position:absolute;top:0.10in;left:0.08in;width:0.86in;height:0.86in" src="ci_1.png"/>--}}
     <div style="position:absolute;top:1.04in;left:0.12in;width:0.82in;line-height:0.12in;"><span
@@ -14,6 +14,6 @@
     <div style="position:absolute;top:1.15in;left:0.12in;width:0.82in;line-height:0.12in;"><span
                 style="font-style:normal;font-weight:normal;font-size:7pt;font-family:HelveticaNeue;color:#000000">{{ $arupian->last_name }} </span></SPAN>
         <br/></div>
-<img style="position:absolute;top:0.00in;left:0.00in;width:1.03in;height:1.42in" src="{{ asset('assets/images/ci_2.png') }}"/>
+{{--<img style="position:absolute;top:0.00in;left:0.00in;width:1.03in;height:1.42in" src="{{ asset('assets/images/ci_2.png') }}"/>--}}
 </body>
 </html>
